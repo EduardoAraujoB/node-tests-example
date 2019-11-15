7a2eae221197dddd6e542b3aa147a82a211c74ec
EduardoAraujoB <ea0123450@gmail.com>
2019-11-15 13:31:21 -0300

    created test and verification for duplicated emails

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +17,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should be not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Eduardo Araujo',
+        email: 'ea0123450@gmail.com',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Eduardo Araujo',
+        email: 'ea0123450@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
