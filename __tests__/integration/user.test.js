3e3a2523b81d99ab01ae25bc19387fbaa3514998
EduardoAraujoB <ea0123450@gmail.com>
2019-11-15 13:59:44 -0300

    created test and functionality to encrypt the password

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,19 +1,34 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
   });
+
+  it('should be encript user password when a new user is created', async () => {
+    const user = await User.create({
+      name: 'Eduardo Araujo',
+      email: 'ea0123450@gmail.com',
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         name: 'Eduardo Araujo',
         email: 'ea0123450@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
@@ -25,7 +40,7 @@ describe('User', () => {
       .send({
         name: 'Eduardo Araujo',
         email: 'ea0123450@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     const response = await request(app)
